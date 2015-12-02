2824aac8aa24f71372622dd02918b3c89d7b6666
Steven Spriggs <steven.spriggs@gmail.com>
2015-12-02 10:51:40 -0500

    update with tests and listing.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var gulp = require('gulp'),
+    fs = require('fs'),
     sass = require('gulp-sass'),
+    scsslint = require('gulp-scss-lint'),
+    sourcemaps = require('gulp-sourcemaps'),
     prefix = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
-    del = require("del"),
-    runSequence = require('run-sequence'),
-    scsslint = require('gulp-scss-lint'),
     handlebars = require('gulp-compile-handlebars'),
-    extend = require('gulp-extend'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
 
 
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-      baseDir: "./build/",
+      baseDir: "./build",
     },
     open: false,
     logConnections: true,
@@ -23,17 +23,48 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('clean', function(cb){
-  return del([
-    'build/**'
-  ], cb);
+gulp.task('build-test-styles', function(){
+  var templateData = JSON.parse(fs.readFileSync('./data/_wvu-variables.json'));
+
+  var options = {};
+
+  return gulp.src('./test/partials/_styles.scss.hbs')
+    .pipe(handlebars(templateData, options))
+    .pipe(rename('_generated-styles.scss'))
+    .pipe(gulp.dest('./test/scss'));
 });
 
-gulp.task('move-dependecies', function() {
-
+gulp.task('compile-scss', ['scss-lint'], function(){
+  return gulp.src([
+      './test/scss/_generated-styles.scss',
+      './test/scss/styles.scss'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: ['scss'],
+      outputStyle: 'expanded'
+    }))
+    .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7", { cascade: true }))
+    .pipe(sourcemaps.write('maps', {
+      includeContent: false,
+      sourceRoot: './build/css/'
+    }))
+    .pipe(gulp.dest('./build/css/'));
 });
 
-gulp.task('scss-lint', function() {
+gulp.task('build', ['compile-scss'], function () {
+
+  var templateData = JSON.parse(fs.readFileSync('./data/_wvu-variables.json'));
+
+  var options = {};
+
+  return gulp.src('./test/index.hbs')
+        .pipe(handlebars(templateData, options))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('scss-lint', ['build-test-styles'], function(){
   return gulp.src('./src/scss/*.scss')
     .pipe(scsslint({
       'config': '.scss-lint.yml'
@@ -41,52 +72,11 @@ gulp.task('scss-lint', function() {
     .pipe(scsslint.failReporter());
 });
 
-gulp.task('compile-scss', function(){
-  return gulp.src([
-      './bower_components/wvu-patterns-masthead-logo/src/scss/*.scss',
-      './bower_components/wvu-patterns-masthead-links/src/scss/*.scss',
-      './src/scss/*.scss'
-    ])
-    .pipe(rename(function (path) {
-      path.basename = path.basename.substring(1)
-    }))
-    .pipe(sass({
-      includePaths: ['scss'],
-      outputStyle: 'expanded'
-    }))
-    .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7", { cascade: true }))
-    .pipe(gulp.dest('./build/css/'));
+gulp.task('ci',['build']);
+
+gulp.task('default',['build','browser-sync'], function(){
+  gulp.watch(["./src/*.scss","./test/scss/*.scss"],["build"]);
+  gulp.watch(["./test/**/*.hbs","./data/*.json"],["build"]);
+  gulp.watch("./build/**/*.html").on('change',reload);
+  gulp.watch("./build/css/*.css").on('change',reload);
 });
-
-gulp.task('build-json',function(){
-  return gulp.src([
-    './src/handlebars/data/*.json',
-    './bower_components/wvu-patterns-masthead-**/src/handlebars/data/*.json'
-    ])
-  .pipe(extend('_wvu-masthead.json',true,2))
-  .pipe(gulp.dest("./build/data"));
-})
-
-gulp.task('compile', ['build-json','scss-lint','compile-scss'], function () {
-
-  var templateData = require('./build/data/_wvu-masthead.json');
-
-  var options = {
-    batch : [
-      './bower_components/wvu-patterns-masthead-logo/src/handlebars/partials',
-      './bower_components/wvu-patterns-masthead-links/src/handlebars/partials',
-      './src/handlebars/partials'
-    ]
-  }
-  return gulp.src('./test/index.hbs')
-        .pipe(handlebars(templateData, options))
-        .pipe(rename('index.html'))
-        .pipe(gulp.dest('./build'));
-});
-
-gulp.task('build',function(cb){
-  runSequence('clean','move-dependecies','compile',cb);
-});
-
-gulp.task('test',['build','browser-sync']);
-gulp.task('ci',['scss-lint']);
